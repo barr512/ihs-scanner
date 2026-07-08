40e86656de702d2daf58915cf42a9c5055a32b40
barr512 <brentshortusda@gmail.com>
2026-07-08 17:18:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -812,3 +812,9 @@ function renderInstructions(plan, input) {
     </button>
   `;
 }
+function ordinal(number) {
+  const suffixes = ["th", "st", "nd", "rd"];
+  const value = number % 100;
+
+  return number + (suffixes[(value - 20) % 10] || suffixes[value] || suffixes[0]);
+}
